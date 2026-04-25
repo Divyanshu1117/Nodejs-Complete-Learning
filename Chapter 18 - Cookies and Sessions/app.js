119696e3df76a6f623776222b78697ea5976c6a3
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2026-04-25 15:42:11 +0530

    Add new programs

--- Chapter 18 - Cookies and Sessions/app.js
+++ Chapter 18 - Cookies and Sessions/app.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 // External Module
 const express = require('express');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const DB_PATH = "mongodb+srv://root:mongodb@completecoding.jdagmyl.mongodb.net/airbnb?appName=CompleteCoding";
 
 //Local Module
 const storeRouter = require("./routes/storeRouter");
@@ -17,10 +20,25 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const store = new MongoDBStore({
+  uri: DB_PATH,
+  collection: 'sessions',
+});
+
 app.use(express.urlencoded());
+
+app.use(session({
+  secret: "KnowledgeGate AI with Complete Coding",
+  resave: false,
+  saveUninitialized: true,
+  // store: store,
+  store
+}));
+
 app.use((req, res, next) => {
   console.log('Cookie check middleware', req.get('Cookie'));
-  req.isLoggedIn = req.get('Cookie') ? req.get('Cookie').split('=') [1] === 'true' : false;
+  // req.isLoggedIn = req.get('Cookie') ? req.get('Cookie').split('=')[1] === 'true' : false;
+  req.isLoggedIn = req.session.isLoggedIn;
   next();
 });
 
@@ -39,7 +57,6 @@ app.use(express.static(path.join(rootDir, 'public')))
 app.use(errorsController.pageNotFound);
 
 const PORT = 3000;
-const DB_PATH = "mongodb+srv://root:mongodb@completecoding.jdagmyl.mongodb.net/airbnb?appName=CompleteCoding";
 
 mongoose.connect(DB_PATH).then(() => {
   console.log('Connected to Mongo');
