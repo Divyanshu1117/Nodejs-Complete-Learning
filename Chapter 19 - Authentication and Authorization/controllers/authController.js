44af8c75ad8369493c22ebd1cc63d64df029c297
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2026-04-26 00:11:38 +0530

    add new files

---
+++ Chapter 19 - Authentication and Authorization/controllers/authController.js
@@ -0,0 +1,39 @@
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    pageTitle: "Login",
+    currentPage: "login",
+    isLoggedIn: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    pageTitle: "Signup",
+    currentPage: "signup",
+    isLoggedIn: false,
+    errors: [],
+    oldInput: {},
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  console.log(req.body);
+  // req.session.isLoggedIn = true;
+  // res.cookie("isLoggedIn", true);
+  // req.isLoggedIn = true;
+  res.redirect("/login");
+};
+
+exports.postLogin = (req, res, next) => {
+  console.log(req.body);
+  req.session.isLoggedIn = true;
+  // res.cookie("isLoggedIn", true);
+  // req.isLoggedIn = true;
+  res.redirect("/");
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(() => {
+    res.redirect("/login");
+  });
+};
