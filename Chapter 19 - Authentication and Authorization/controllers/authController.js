55f1f453667eb5ae5441a04700674358fd9d7f7d
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2026-04-27 00:06:01 +0530

    Add new programs

--- Chapter 19 - Authentication and Authorization/controllers/authController.js
+++ Chapter 19 - Authentication and Authorization/controllers/authController.js
@@ -1,3 +1,7 @@
+const { check, validationResult } = require("express-validator");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     pageTitle: "Login",
@@ -12,17 +16,109 @@ exports.getSignup = (req, res, next) => {
     currentPage: "signup",
     isLoggedIn: false,
     errors: [],
-    oldInput: {},
+    oldInput: { firstName: "", lastName: "", email: "", userType: "" },
   });
 };
 
-exports.postSignup = (req, res, next) => {
-  console.log(req.body);
-  // req.session.isLoggedIn = true;
-  // res.cookie("isLoggedIn", true);
-  // req.isLoggedIn = true;
-  res.redirect("/login");
-};
+exports.postSignup = [
+  check("firstName")
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage("First name should be atleast 2 characters long")
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage("First name should contain only alphabets"),
+
+  check("lastName")
+    .matches(/^[a-zA-Z]*$/)
+    .withMessage("Last name should contain only alphabets"),
+
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .normalizeEmail(),
+
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password should be atleast 8 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password should contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password should contain at least one lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password should contain at least one number")
+    .matches(/[!@&]/)
+    .withMessage("Password should contain at least one special character (!, @, &)")
+    .trim(),
+
+  check("confirmPassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
+
+  check("userType")
+    .notEmpty()
+    .withMessage("User type is required")
+    .isIn(['guest', 'host'])
+    .withMessage("Invalid user type"),
+
+  check("terms")
+    .notEmpty()
+    .withMessage("Please accept the terms and conditions")
+    .custom((value, { req }) => {
+      if (value !== "on") {
+        throw new Error("You must accept the terms and conditions");
+      }
+      return true;
+    }),
+
+  (req, res, next) => {
+    console.log(req.body);
+    const { firstName, lastName, email, password, userType } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).render("auth/signup", {
+        pageTitle: "Signup",
+        currentPage: "signup",
+        isLoggedIn: false,
+        errors: errors.array().map(err => err.msg),
+        oldInput: { firstName, lastName, email, password, userType },
+      });
+    }
+
+    bcrypt.hash(password, 12)
+      .then(hashedPassword => {
+        const user = new User({ firstName, lastName, email, password: hashedPassword, userType });
+        return user.save();
+      })
+      .then(() => {
+        res.redirect("/login");
+      }).catch(err => {
+        return res.status(422).render("auth/signup", {
+          pageTitle: "Signup",
+          currentPage: "signup",
+          isLoggedIn: false,
+          errors: [err.message],
+          oldInput: { firstName, lastName, email, password, userType },
+        });
+      });
+
+    // const user = new User({ firstName, lastName, email, password, userType });
+    // user.save().then(() => {
+    //   res.redirect("/login");
+    // }).catch(err => {
+    //   return res.status(422).render("auth/signup", {
+    //     pageTitle: "Signup",
+    //     currentPage: "signup",
+    //     isLoggedIn: false,
+    //     errors: [err.message],
+    //     oldInput: { firstName, lastName, email, password, userType },
+    //   });
+    // });
+  }];
 
 exports.postLogin = (req, res, next) => {
   console.log(req.body);
